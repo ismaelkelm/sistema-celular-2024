b8e7d719af0733b6a7c217e233d6950aca01c6a4
arielon23 <129569305+arielon23@users.noreply.github.com>
2024-08-22 02:40:14 -0300

    ..

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="es" >
+<html lang="es">
 
 <head>
     <meta charset="UTF-8">
@@ -14,36 +14,39 @@
 
 <body class="vh-100 overflow-hidden">
     <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg fixed-top bg-transparent">
+    <nav class="navbar navbar-expand-lg">
         <div class="container">
             <!-- Logo -->
-            <a class="navbar-brand " href="#">Marquez Comunicaciones</a>
+            <a class="navbar-brand fs-4 " href="#">Mar quez Comunicaciones</a>
             <img src="" alt="" width="" height="" alt="">
 
             <!-- Toggle Button -->
 
-            <button class="navbar-toggler  border-0" type="button"
+            <button class="navbar-toggler shadow-none border-0 "
+                type="button"
                 data-bs-toggle="offcanvas"
                 data-bs-target="#offcanvasNavbar"
                 aria-controls="offcanvasNavbar">
                 <span class="navbar-toggler-icon"></span>
             </button>
+
             <!-- Sidebar -->
             <div class="sidebar offcanvas offcanvas-start"
                 tabindex="-1"
                 id="offcanvasNavbar"
                 aria-labelledby="offcanvasNavbarLabel">
                 <!-- Sidebar Header -->
-                <div class="offcanvas-header  border-bottom">
+                <div class="offcanvas-header text-white border-bottom">
                     <h5 class="offcanvas-title"
                         id="offcanvasNavbarLabel">Menú</h5>
                     <button
                         type="button"
-                        class="btn-close btn-close-white"
+                        class="btn-close btn-close-white shadow-none"
                         data-bs-dismiss="offcanvas"
                         aria-label="Close">
                     </button>
                 </div>
+                <!-- Sidebar body -->
                 <div class="offcanvas-body">
                     <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
                         <li class="nav-item">
